0ba3edb5d6a99d03555ab1878713d2fa67662e2c
Jørn Efteland <joreft@gmail.com>
2020-04-11 20:01:34 +0200

    Started implementing a training algorithm

--- main.cpp
+++ main.cpp
@@ -1,9 +1,9 @@
 #include <fmt/format.h>
 #include <thread>
 #include "neuroticpp.h"
-
+#include <algorithm>
 #include "sdl_abstraction.h"
-
+#include <future>
 #include "mnist_parser.h"
 
 
@@ -26,33 +26,106 @@ int calculate_best_guess(jeagle::Layer<10, 0> const& final_layer)
     return max_element_at - final_layer.activation_values.begin() + 1;
 }
 
+struct LabelImageZip
+{
+    Image image;
+    int label;
+};
+
+std::vector<LabelImageZip> zip(std::vector<Image> const& images, std::vector<int> const& labels)
+{
+    std::vector<LabelImageZip> out;
+    out.reserve(images.size());
+    for (std::size_t i = 0; i < images.size(); ++i)
+    {
+        out.emplace_back(LabelImageZip{images.at(i), labels.at(i)});
+    }
+
+    return out;
+}
+
+using namespace jeagle;
+void train_network(jeagle::SampleNetwork& net, std::vector<Image> const& images, std::vector<int> const& labels)
+{
+    if (labels.size() != images.size()) throw "panic at the disco";
+
+    auto zipped = zip(images, labels);
+
+    std::random_device rd;
+    std::mt19937 g(rd());
+
+    std::shuffle(zipped.begin(), zipped.end(), g);
+
+    auto const  batch_size = zipped.size() / 40; // only take the first 1/40 of the randomly shuffled samples
+
+    std::vector<float> costs{};
+
+    for (std::size_t i = 0; i < batch_size; ++i)
+    {
+        auto const& image = zipped.at(i).image;
+        auto const target = zipped.at(i).label;
+
+        net.process_input(image_to_first_layer_activation_values(image));
+        auto const& results = net.final_layer.activation_values;
+        float cost = 0;
+        for (int u = 0; u < 10; ++u)
+        {
+            if (u == target)
+            {
+                cost += std::pow(results.at(u) - 1.0f, 2.f);
+            }
+            else
+            {
+                cost += std::pow(results.at(u), 2.f);
+            }
+        }
+
+        costs.emplace_back(cost);
+    }
+    // Mean squared error (MSE) / Quadratic cost
+    auto const average_cost = std::accumulate(costs.begin(), costs.end(), 0.f) / (2.f * batch_size);
+    fmt::print("Final cost was {}\n", average_cost);
+
+}
+
 int main()
 {//1262459412
     srand(std::chrono::system_clock::to_time_t(std::chrono::system_clock::now()));
     std::cout << "First random = " << rand() << "\n";
     auto const training_labels = read_label_file("assets/train-labels-idx1-ubyte");
-    auto const images = parse_images_file("assets/train-images-idx3-ubyte");
+    auto const training_images = parse_images_file("assets/train-images-idx3-ubyte");
 
     fmt::print("Training labels size = {}\n", training_labels.size());
-    fmt::print("Images file size = {}\n", images.size());
+    fmt::print("Images file size = {}\n", training_images.size());
 
     jeagle::SampleNetwork neural_net {};
 
+    auto const start_time = std::chrono::system_clock::now();
+    train_network(neural_net, training_images, training_labels);
+    auto const train_time = std::chrono::system_clock::now() - start_time;
+
+    auto const elapsed_seconds = std::chrono::duration_cast<std::chrono::milliseconds>(train_time).count();
+    fmt::print("Training finished in {}ms\n", elapsed_seconds);
+
+    return 0;
     SDLAbstraction sdl_handle;
     InputState input {};
+
+    auto const test_labels = read_label_file("assets/t10k-labels-idx1-ubyte");
+    auto const test_images = parse_images_file("assets/t10k-images-idx3-ubyte");
 
     std::size_t image_index = 0;
     while (!input.should_quit)
     {
         if (input.advance_image)
         {
-            if (image_index == images.size()) break;
+            if (image_index == test_images.size()) break;
             ++image_index;
             input.advance_image = false;
         }
 
-        auto const& image = images[image_index];
-        auto const supposed_number = training_labels[image_index];
+        auto const& image = test_images[image_index];
+        auto const supposed_number = test_labels[image_index];
 
         neural_net.process_input(image_to_first_layer_activation_values(image));
 
